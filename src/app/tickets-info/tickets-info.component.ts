cf2ddf99e30d5885ead98aae89a41bc3e8c733d8
Gerardo Arjona <gerardoarjona@Gerardos-MacBook-Pro.local>
2018-11-20 19:10:15 -0600

    Add ticket info card
    Signed-off-by: Gerardo Arjona <gerardoarjona@Gerardos-MacBook-Pro.local>

--- src/app/tickets-info/tickets-info.component.ts
+++ src/app/tickets-info/tickets-info.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TicketsInfoComponent implements OnInit {
 
+  buttonText = 'Editar';
+  buttonState = true;
+  typeInput = 'password';
+  isReadOnly = true;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onClickButton(nombre){
+    console.log(nombre);
+    if (this.buttonState == true){
+      this.isReadOnly = false;
+      this.buttonText = 'Guardar';
+      this. typeInput = 'text'
+      this.buttonState = false;
+    } else {
+      this.buttonState = true;
+      this.isReadOnly = true;
+      this.typeInput = 'password';
+      this.buttonText = 'Editar';
+    }
   }
 
 }
