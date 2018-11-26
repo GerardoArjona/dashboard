4cf5b115579a0c11b6d33075fe2909e62ff8cca9
Gerardo Arjona <gerardo.arj15@gmail.com>
2018-11-26 15:09:56 -0600

    Add user chat component & fix pagination buttons at ticket list table CSS
    Signed-off-by: Gerardo Arjona <gerardo.arj15@gmail.com>

--- src/app/user-chat/user-chat.component.ts
+++ src/app/user-chat/user-chat.component.ts
@@ -7,6 +7,152 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserChatComponent implements OnInit {
 
+  user = {
+    name: "Gerardo"
+  }
+
+  messages = [
+    {
+      text: "Hola", 
+      date: "12-03-24",
+      time: "12:00:00",
+      who: "received text-left pull-left"
+    },
+    {
+      text: "Hola", 
+      date: "12-03-24",
+      time: "12:00:00",
+      who: "sent text-right pull-right"
+    },
+    {
+      text: "Hola", 
+      date: "12-03-24",
+      time: "12:00:00",
+      who: "sent text-right pull-right"
+    },
+    {
+      text: "Hola", 
+      date: "12-03-24",
+      time: "12:00:00",
+      who: "sent text-right pull-right"
+    },
+    {
+      text: "Hola", 
+      date: "12-03-24",
+      time: "12:00:00",
+      who: "sent text-right pull-right"
+    },
+    {
+      text: "Hola", 
+      date: "12-03-24",
+      time: "12:00:00",
+      who: "sent text-right pull-right"
+    },
+    {
+      text: "Hola", 
+      date: "12-03-24",
+      time: "12:00:00",
+      who: "sent text-right pull-right"
+    },
+    {
+      text: "Hola", 
+      date: "12-03-24",
+      time: "12:00:00",
+      who: "received text-left pull-left"
+    },
+    {
+      text: "Hola", 
+      date: "12-03-24",
+      time: "12:00:00",
+      who: "received text-left pull-left"
+    },
+    {
+      text: "Hola", 
+      date: "12-03-24",
+      time: "12:00:00",
+      who: "received text-left pull-left"
+    },
+    {
+      text: "Hola", 
+      date: "12-03-24",
+      time: "12:00:00",
+      who: "received text-left pull-left"
+    },
+    {
+      text: "Hola", 
+      date: "12-03-24",
+      time: "12:00:00",
+      who: "received text-left pull-left"
+    },
+    {
+      text: "Hola", 
+      date: "12-03-24",
+      time: "12:00:00",
+      who: "received text-left pull-left"
+    },
+    {
+      text: "Hola", 
+      date: "12-03-24",
+      time: "12:00:00",
+      who: "received text-left pull-left"
+    },
+    {
+      text: "Hola", 
+      date: "12-03-24",
+      time: "12:00:00",
+      who: "received text-left pull-left"
+    },
+    {
+      text: "Hola", 
+      date: "12-03-24",
+      time: "12:00:00",
+      who: "received text-left pull-left"
+    },
+    {
+      text: "Hola", 
+      date: "12-03-24",
+      time: "12:00:00",
+      who: "received text-left pull-left"
+    },
+    {
+      text: "Hola", 
+      date: "12-03-24",
+      time: "12:00:00",
+      who: "received text-left pull-left"
+    },
+    {
+      text: "Hola", 
+      date: "12-03-24",
+      time: "12:00:00",
+      who: "received text-left pull-left"
+    },
+    {
+      text: "Hola", 
+      date: "12-03-24",
+      time: "12:00:00",
+      who: "received text-left pull-left"
+    },
+    {
+      text: "Hola", 
+      date: "12-03-24",
+      time: "12:00:00",
+      who: "received text-left pull-left"
+    },
+    {
+      text: "Hola", 
+      date: "12-03-24",
+      time: "12:00:00",
+      who: "received text-left pull-left"
+    },
+    {
+      text: "Hola", 
+      date: "12-03-24",
+      time: "12:00:00",
+      who: "received text-left pull-left"
+    },
+
+  ];
+
   constructor() { }
 
   ngOnInit() {
